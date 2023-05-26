930ac2beee3b8c88d26c503f2d88359f9fe00345
Onkar58 <onkarwaghmode58@gmail.com>
2023-05-26 12:52:05 +0530

    footer made desktop version

--- src/App.jsx
+++ src/App.jsx
@@ -26,22 +26,22 @@ const App = () => {
     <>
       <ThemeContext.Provider value={{ theme, toggleTheme }}>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login-signup" element={<LoginSignupPage />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route
-            path="/projects/:projectId"
-            element={<IndividualProjectPage />}
-          />
-          <Route path="/team" element={<TeamPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/testing" element={<TestingPage />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-        <Chatbotfolder />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login-signup" element={<LoginSignupPage />} />
+            <Route path="/events" element={<EventsPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route
+              path="/projects/:projectId"
+              element={<IndividualProjectPage />}
+            />
+            <Route path="/team" element={<TeamPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/testing" element={<TestingPage />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+          <Chatbotfolder />
         <Footer />
       </ThemeContext.Provider>
     </>
