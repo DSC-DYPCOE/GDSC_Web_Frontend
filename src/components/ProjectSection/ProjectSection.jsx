35adbcfbd455b3214dfb9076691643795035999c
Shweta2003 <blossomshweta2003@gmail.com>
2023-08-21 20:11:58 +0530

    my commit

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,18 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 // import ProjectCard from '../common/ProjectCard/ProjectCard'
 import info from './ProjectInfo'
 import classes from './ProjectSection.module.css'
 import { ThemeContext } from '../../App'
 import ProjectCard2 from '../common/ProjectCard2/ProjectCard2'
+import left from '../../assets/leftside.png'
+import right from '../../assets/rightside.png'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const ProjectSection = () => {
+    useEffect(() => {
+        AOS.init({duration: 500});
+      },[]);
     const theme = useContext(ThemeContext);
     return (
         <>
             <div className={`${classes.mainDiv} ${theme.theme === "dark" ? classes.dark : "" }`}>
-            <h1>Our Projects</h1>
+            <div className={classes.d}>
+                <img src={left} className={classes.lclass} alt='' data-aos="fade-left"/>
+                <p className={classes.head} alt="" data-aos="fade-in" data-aos-delay="300">Our Projects</p>
+                <img src={right} className={classes.rclass} alt='' data-aos="fade-right"/>
+            </div>
             <div className={classes.container}>
-                {info.map((current) => <ProjectCard2 current={current} />)}
+                {info.map((current) => <ProjectCard2 current={current}/>)}
             </div>
             </div>
         </>
--- src/components/common/ProjectCard2/ProjectCard2.jsx
+++ src/components/common/ProjectCard2/ProjectCard2.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import classes from './ProjectCard2.module.css'
 import Button from '../Button/Button'
 import image from '../../../assets/projectBg.png'
@@ -6,11 +6,16 @@ import { Link } from 'react-router-dom'
 import LinkIcon from '../SVGs/Link'
 import GithubIcon from '../SVGs/Github'
 import { ThemeContext } from '../../../App'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const ProjectCard2 = ({ current }) => {
+    useEffect(() => {
+        AOS.init({duration: 500});
+      },[]);
     const theme = useContext(ThemeContext);
     return (
-        <div className={`${classes.main} ${theme.theme === "dark" ? classes.dark : ""}`} style={{ "--accent-color": current.color }}>
+        <div className={`${classes.main} ${theme.theme === "dark" ? classes.dark : ""}`} style={{ "--accent-color": current.color }} data-aos="flip-left">
             <div className={classes.tags}>
                 {current.authors.map((authorName, index) => <div className={classes.tag}><img src={current.tagImg[index]} alt="Logo Img" /> {authorName}</div>)}
             </div>
